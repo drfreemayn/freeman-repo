201576dea2b8baa6ee65362c532b1e8b2a39e4cf
Fredrik Walterson <fredrik.walterson@gmail.com>
2016-06-11 14:51:55 +0200

    working version with a single gif

--- sex-dice/sex_dice.py
+++ sex-dice/sex_dice.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 #-*- coding: UTF-8 -*-
 
-import time
-import random
 import pygame
+import random
+import weakref
+from GIFImage import GIFImage
 from math import ceil, exp
 
 # mouse ids
@@ -22,6 +23,13 @@ NUM_DICE_SIDES = 6
 DICE_WIDTH = 128
 DICE_HEIGHT = 128
 
+# colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+LIGHT_GRAY = (130, 130, 130)
+DARK_GRAY = (100, 100, 100)
+RED = (255, 0, 0)
+
 DICE_ARRAY = [[0, 0, DICE_WIDTH, DICE_HEIGHT],
               [DICE_WIDTH, 0, DICE_WIDTH, DICE_HEIGHT],
               [2*DICE_WIDTH, 0, DICE_WIDTH, DICE_HEIGHT],
@@ -31,6 +39,12 @@ DICE_ARRAY = [[0, 0, DICE_WIDTH, DICE_HEIGHT],
               
 FIBONACCI_ARRAY = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+POSITIONS = ['Anal sex',
+             'Missionary',
+             '69',
+             'The Spoon',
+             'Doggystyle',
+             'Cowgirl']
 
 class Text(pygame.sprite.DirtySprite):
 
@@ -54,7 +68,7 @@ class Text(pygame.sprite.DirtySprite):
         self.rect.topleft = (x, y)
 
     def update(self):
-        pass #do something here
+        self.dirty = 1
 
         
 class Image(pygame.sprite.DirtySprite):
@@ -72,14 +86,61 @@ class Image(pygame.sprite.DirtySprite):
         self.rect.topleft = (x, y)
 
     def update(self):
-        pass #do something here
+        self.dirty = 1
+        
+class Popup(pygame.sprite.DirtySprite):
+    
+    def __init__(self, parent):
+        # Call the parent class (Sprite) constructor
+        pygame.sprite.DirtySprite.__init__(self)
+        
+        # store ref to parent object
+        self.parent = weakref.ref(parent)    # <= garbage-collector safe!
+        
+        self.gif = GIFImage("gifs\cowgirl.gif")
+        w = self.gif.get_width()
+        h = self.gif.get_height()
+        x = parent.screen.get_width()/2 - w/2
+        y = parent.screen.get_height()/2 - h/2
+        
+        # create an image surface
+        self.image = pygame.Surface((w, h))
+
+        # set pos
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        
+        # set popup status
+        self.visible = 0
+    
+    def isShowing(self):
+        if self.visible == 1:
+            return True
+        return False
+    
+    def show(self):
+        self.visible = 1
+        self.dirty = 1
+    
+    def close(self):
+        self.visible = 0
+        self.dirty = 1
+    
+    def update(self):
+        self.dirty = 0
+        if self.visible == 1:
+            self.gif.render(self.image, (0,0))
+            self.dirty = 1
   
   
 class Button(pygame.sprite.DirtySprite):
 
-    def __init__(self, x, y, w, h):
+    def __init__(self, x, y, w, h, parent):
         # Call the parent class (Sprite) constructor
         pygame.sprite.DirtySprite.__init__(self)
+        
+        # store ref to parent object
+        self.parent = weakref.ref(parent)    # <= garbage-collector safe!
 
         # define button
         # note that buttonRect is not in global coordinates
@@ -89,10 +150,10 @@ class Button(pygame.sprite.DirtySprite):
                                       border_size,
                                       w-2*border_size,
                                       h-2*border_size)
-        self.buttonColorUp = pygame.Color(130, 130, 130)
-        self.buttonColorDown = pygame.Color(100, 100, 100)
-        self.borderColor = pygame.Color("white")
-
+        self.buttonColorUp = LIGHT_GRAY
+        self.buttonColorDown = DARK_GRAY
+        self.borderColor = WHITE
+        
         # create an image surface
         self.image = pygame.Surface([w, h])
         
@@ -100,19 +161,19 @@ class Button(pygame.sprite.DirtySprite):
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
         
-        self.draw(self.buttonColorUp)
+        self.__draw(self.buttonColorUp)
         
         # state
         self.pressed = False
     
-    def draw(self, color):
+    def __draw(self, color):
         self.image.fill(self.borderColor)
         self.image.fill(color, self.buttonRect)
         
         # add label text
         text = 'Throw dice'
         size = 24
-        color = pygame.Color("red")
+        color = RED
         antialias = 1
         font = pygame.font.SysFont("Arial", size)
         textSurf = font.render(text, antialias, color)
@@ -120,27 +181,32 @@ class Button(pygame.sprite.DirtySprite):
         H = textSurf.get_height()
         label_pos = (self.buttonRect.w/2 - W/2, self.buttonRect.h/2 - H/2)
         self.image.blit(textSurf, label_pos)
-        # label = Text(text, size, color, self.rect.x, self.rect.y)        
+        
+    def update(self):
+        self.dirty = 1
 
-    def on_press(self, dice, pos):
-        x, y = pos
+    def on_press(self, func):
+        x, y = pygame.mouse.get_pos()
         rect = self.rect
         if (rect.x <= x) and (x <= (rect.x+rect.w)) \
            and (rect.y <= y) and (y <= rect.y+rect.h) \
            and not self.pressed:
             self.pressed = True
-            dice.throw_dice()
-            self.draw(self.buttonColorDown)
+            func()
+            self.__draw(self.buttonColorDown)
 
     def on_release(self):
         self.pressed = False
-        self.draw(self.buttonColorUp)
+        self.__draw(self.buttonColorUp)
             
-class Dice(Image):
+class Dice(pygame.sprite.DirtySprite):
     
-    def __init__(self, x, y, size):
-        # Call the parent class (Image) constructor
+    def __init__(self, x, y, size, parent):
+        # Call the parent class constructor
         pygame.sprite.DirtySprite.__init__(self)
+        
+        # store ref to parent object
+        self.parent = weakref.ref(parent)    # <= garbage-collector safe!
         
         dice_path = "sides.jpg"
         self.img_orig = pygame.image.load(dice_path).convert()
@@ -154,120 +220,159 @@ class Dice(Image):
         self.thrown = False
         self.frame_counter = 0
         self.num_counter = 0
-        self.set_random_number()
+        self.__set_random_number()
 
-    def set_random_number(self):
-        num = self.generate_outcome()
+    def __set_random_number(self):
+        num = self.__generate_outcome()
         rect = DICE_ARRAY[num-1]
         side_img = self.img_orig.subsurface(rect[0], rect[1], rect[2], rect[3])
         self.image = pygame.transform.scale(side_img, (self.size, self.size))
 
-    def generate_outcome(self):
+    def __generate_outcome(self):
         rng_num = random.random()
         outcome = int(ceil(NUM_DICE_SIDES * rng_num))
         return outcome
+    
+    def __update_dice(self):
+        finished = False
+        if (FIBONACCI_ARRAY[self.num_counter] == self.frame_counter):
+            self.frame_counter = 0
+            self.__set_random_number()
+            if (self.num_counter == (len(FIBONACCI_ARRAY)-1)):
+                self.__reset()
+                finished = True
+            else:
+                self.num_counter += 1
+        else:
+            self.frame_counter += 1
+        
+        return finished
 
-    def reset(self):
+    def __reset(self):
         self.thrown = False
         self.num_counter = 0
         self.frame_counter = 0    
 
     def update(self):
+        self.dirty = 1
         if self.thrown:
-            if (FIBONACCI_ARRAY[self.num_counter] == self.frame_counter):
-                self.frame_counter = 0
-                self.set_random_number()
-                if (self.num_counter == (len(FIBONACCI_ARRAY)-1)):
-                    self.reset()
-                else:
-                    self.num_counter += 1
-            else:
-                self.frame_counter += 1
+            finished = self.__update_dice()
+            if (finished):
+                self.parent().popup.show()
 
     def throw_dice(self):
         self.thrown = True
+        
 
+class GameApp(object):
+    
+    def __init__(self):
+        # initialize game
+        pygame.init()
+        pygame.font.init()
+        logo = pygame.image.load('logo.jpg')
+        pygame.display.set_icon(logo)
+        pygame.display.set_caption('Sex dice')
+        self.screen = pygame.display.set_mode((WINDOW_WIDTH-PADDING,
+                                               WINDOW_HEIGHT-PADDING))
+        self.running = False
+
+        # crate game label
+        font_size = 48
+        text = "SEX DICE"
+        label_color = RED
+        label_x = 50
+        label_y = PADDING
+        self.label = Text(text,
+                          font_size,
+                          label_color,
+                          label_x,
+                          label_y)
+
+        # create default image
+        sex_path = "sex.jpg"
+        sex_w = (WINDOW_WIDTH/2) - 2*PADDING
+        sex_h = (WINDOW_HEIGHT/2) - 2*PADDING
+        sex_x = WINDOW_WIDTH/2
+        sex_y = PADDING
+        self.sex_img = Image(sex_path,
+                             sex_x,
+                             sex_y,
+                             sex_w,
+                             sex_h)
+
+        # create game button
+        rect_w = WINDOW_WIDTH - 3*PADDING
+        rect_h = WINDOW_HEIGHT/2 - 2*PADDING
+        rect_x = PADDING
+        rect_y = WINDOW_HEIGHT/2
+        self.button = Button(rect_x,
+                             rect_y,
+                             rect_w,
+                             rect_h,
+                             self)
+
+        # create dice
+        dice_x = 90
+        dice_y = 85
+        dize_size = 128
+        self.dice = Dice(dice_x,
+                         dice_y,
+                         dize_size,
+                         self)
+                    
+        # create a gif popup
+        self.popup = Popup(self)
+
+        # define render group
+        self.render_group = pygame.sprite.RenderUpdates()
+        self.render_group.add(self.button,
+                              self.label,
+                              self.dice,
+                              self.sex_img)
+                              
+        self.popup_group = pygame.sprite.RenderUpdates()
+        self.popup_group.add(self.popup)
+        
+    def __clean_screen(self):
+        self.screen.fill(BLACK)
+        pygame.display.update()
+                              
+    def __processEvent(self, event):
+        if event.type == pygame.QUIT:
+            self.running = False
+        if (event.type == pygame.KEYDOWN) and \
+            self.popup.isShowing():
+            self.popup.close()
+            self.__clean_screen()
+        if (event.type == pygame.MOUSEBUTTONDOWN) and \
+           (event.button == LEFT_BUTTON):
+            self.button.on_press(self.dice.throw_dice)
+        elif event.type == pygame.MOUSEBUTTONUP and \
+            event.button == LEFT_BUTTON:
+            self.button.on_release()
+        
+    def run(self):
+        # main loop
+        clock = pygame.time.Clock()
+        self.running = True
+        while self.running:
+            dt = clock.tick(FRAMES_PER_SECOND)
+            #print "Time lapsed: {0}".format(dt)
+            for event in pygame.event.get():
+                self.__processEvent(event)
+                    
+            # update view
+            if self.popup.isShowing():
+                self.popup_group.update()
+                pygame.display.update(self.popup_group.draw(self.screen))
+            else:
+                self.render_group.update()
+                pygame.display.update(self.render_group.draw(self.screen))
 
 def main():
-    # initialize game
-    pygame.init()
-    pygame.font.init()
-    clock = pygame.time.Clock()
-    logo = pygame.image.load('logo.jpg')
-    pygame.display.set_icon(logo)
-    pygame.display.set_caption('Sex dice')
-    screen = pygame.display.set_mode((WINDOW_WIDTH-PADDING,
-                                      WINDOW_HEIGHT-PADDING))
-
-    # crate game label
-    font_size = 48
-    text = "SEX DICE"
-    label_color = pygame.Color("red")
-    label_x = 50
-    label_y = PADDING
-    label = Text(text,
-                 font_size,
-                 label_color,
-                 label_x,
-                 label_y)
-
-    # create default image
-    sex_path = "sex.jpg"
-    sex_w = (WINDOW_WIDTH/2) - 2*PADDING
-    sex_h = (WINDOW_HEIGHT/2) - 2*PADDING
-    sex_x = WINDOW_WIDTH/2
-    sex_y = PADDING
-    sex_img = Image(sex_path,
-                    sex_x,
-                    sex_y,
-                    sex_w,
-                    sex_h)
-
-    # create game button
-    rect_w = WINDOW_WIDTH - 3*PADDING
-    rect_h = WINDOW_HEIGHT/2 - 2*PADDING
-    rect_x = PADDING
-    rect_y = WINDOW_HEIGHT/2
-    button = Button(rect_x,
-                    rect_y,
-                    rect_w,
-                    rect_h)
-
-    # create dice
-    dice_x = 90
-    dice_y = 85
-    dize_size = 128
-    dice = Dice(dice_x,
-                dice_y,
-                dize_size)
-                
-    # define render group
-    render_group = pygame.sprite.RenderUpdates()
-    render_group.add(button, sex_img, label, dice)
-
-    # main loop
-    running = True
-    while running:
-        dt = clock.tick(FRAMES_PER_SECOND)
-        #print "Time lapsed: {0}".format(dt)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN and \
-               event.button == LEFT_BUTTON:
-                mouse_pos = pygame.mouse.get_pos()
-                button.on_press(dice, mouse_pos)
-
-            elif event.type == pygame.MOUSEBUTTONUP and \
-                 event.button == LEFT_BUTTON:
-                button.on_release()
-                    
-                
-        # update view
-        render_group.update()
-        pygame.display.update(render_group.draw(screen))
-            
-    
+    app = GameApp()
+    app.run()
 
 if __name__ == "__main__":
     main()
